c32d9026ddcd7c66f0f02a8ff295c31e39a29ff6
michaelwenk <michael.wenk@uni-jena.de>
2018-07-23 23:36:17 +0200

    - 	new added and modified by casekit.model - 	now in usage at Spectrum class

--- src/NMR/Signal.java
+++ src/NMR/Signal.java
@@ -29,31 +29,85 @@ package NMR;
  */
 public class Signal {
     
-    private final String element;
-    private final String multiplicity;
-    private final double shift;
-    private final Double intensity;
-    
-    public Signal(final String elem, final double shift, final String mult, final Double intens){
-        this.element = elem;
+    private final int ndim;
+
+    /**
+     * Am array of doubles to store the chemical shift of
+     */
+    private final Double shift[];
+    private final String[] nuclei;
+
+    /* Signal intensity in arbitrary values */
+    private Double intensity;
+    private String multiplicity;
+
+    private Integer phase;
+    public final static int DIM_ONE = 1, DIM_TWO = 2, DIM_THREE = 3, DIM_FOUR = 4;
+    public final static int SHIFT_PROTON = 0, SHIFT_HETERO = 1;
+    public final static int PHASE_NONE = 0, PHASE_POSITIVE = 1, PHASE_NEGATIVE = 2;
+    public final static String[] PHASENAMES = {"NONE", "POSITIVE", "NEGATIVE"};
+
+    public Signal(final String[] nuclei, final Double[] shift, final Double intensity, final Integer phase, final String multiplicity) {
+        this.nuclei = nuclei;
+        this.ndim = this.nuclei.length;
         this.shift = shift;
-        this.multiplicity = mult;
-        this.intensity = intens;
+        this.intensity = intensity;
+        this.phase = phase;
+        this.multiplicity = multiplicity;
     }
     
-    public String getElement(){
-        return this.element;
+    public int getDim(){
+        return this.ndim;
     }
     
-    public double getShift(){
-        return this.shift;
+    public void setShift(final Double shift, final int dim) {
+        this.shift[dim] = shift;
+    }
+
+    public Double getShift(final int dim) {
+        return this.shift[dim];
     }
     
-    public String getMultiplicity(){
+    public void setIntensity(final Double intensity) {
+        this.intensity = intensity;
+    }
+
+    public Double getIntensity() {
+        return this.intensity;
+    }
+    
+    public void setMultiplicity(final String multiplicity) {
+        this.multiplicity = multiplicity;
+    }
+
+    public String getMultiplicity() {
         return this.multiplicity;
     }
+
+    public void setPhase(final int phase) {
+        this.phase = phase;
+    }
     
-    public Double getIntensity(){
-        return this.intensity;
+    public Integer getPhase() {
+        return this.phase;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        s += ndim + " -dimensional NMRSignal for nuclei ";
+        for (int f = 0; f < this.nuclei.length; f++) {
+            s += this.nuclei[f] + "; ";
+        }
+        s += "\nShiftlist: ";
+        for (int f = 0; f < this.shift.length; f++) {
+            s += this.shift[f] + "; ";
+        }
+        s += "\n\n";
+        return s;
+    }
+    
+    public String[] getNuclei(){
+        return this.nuclei;
     }
 }

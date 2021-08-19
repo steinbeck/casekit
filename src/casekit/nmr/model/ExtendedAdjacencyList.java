e03a4040db8211e0e20b4470627bf46aebc70548
michaelwenk <michael.wenk@uni-jena.de>
2021-08-19 09:57:54 +0200

    chore: optimisation of ExtendedAdjacencyList

--- src/casekit/nmr/model/ExtendedAdjacencyList.java
+++ src/casekit/nmr/model/ExtendedAdjacencyList.java
@@ -19,7 +19,7 @@ import lombok.Setter;
 import org.openscience.cdk.graph.matrix.ConnectionMatrix;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
-import org.openscience.cdk.interfaces.IAtomType.Hybridization;
+import org.openscience.cdk.interfaces.IAtomType;
 import org.openscience.cdk.interfaces.IBond;
 import org.openscience.cdk.silent.Atom;
 import org.openscience.cdk.silent.Bond;
@@ -39,16 +39,12 @@ import java.util.List;
 @Setter
 public class ExtendedAdjacencyList {
 
-    private int[][][] adjacencyList; // connected atom index, bond order, bond is in ring, bond is aromatic
-    private String[] atomTypes;
-    private Integer[][] atomProperties;// hydrogenCounts, valencies, formalCharges, isInRingAtoms, isAromaticAtoms
-    private Hybridization[] hybridizations;
-    private int bondCount;
-
+    private int[][][] bondProperties; // connected atom index, bond order, bond is in ring, bond is aromatic
+    private Integer[][] atomProperties; // element symbol, hybridization, implicitHydrogenCount, valency, formalCharge, isInRingAtom, isAromaticAtom
 
     public ExtendedAdjacencyList(final IAtomContainer ac) {
         final double[][] connectionMatrix = ConnectionMatrix.getMatrix(ac);
-        this.adjacencyList = new int[connectionMatrix.length][][];
+        this.bondProperties = new int[connectionMatrix.length][][];
         List<int[]> connectedAtomsList;
         int[][] temp;
         IBond bond;
@@ -72,88 +68,82 @@ public class ExtendedAdjacencyList {
                     < connectedAtomsList.size(); k++) {
                 temp[k] = connectedAtomsList.get(k);
             }
-            this.adjacencyList[i] = temp;
+            this.bondProperties[i] = temp;
         }
-        this.atomTypes = new String[this.adjacencyList.length];
-        this.hybridizations = new Hybridization[this.adjacencyList.length];
-        this.atomProperties = new Integer[this.adjacencyList.length][];
+        this.atomProperties = new Integer[this.bondProperties.length][];
 
         IAtom atom;
         for (int i = 0; i
-                < this.adjacencyList.length; i++) {
+                < this.bondProperties.length; i++) {
             atom = ac.getAtom(i);
-            this.setAtomProperties(i, atom.getSymbol(), atom.getImplicitHydrogenCount(), atom.getValency(),
-                                   atom.getFormalCharge(), atom.isInRing(), atom.isAromatic(), atom.getHybridization());
+            this.atomProperties[i] = new Integer[7];
+            this.atomProperties[i][0] = atom.getSymbol()
+                                            .equals("R")
+                                        ? -1
+                                        : atom.getAtomicNumber();
+            this.atomProperties[i][1] = atom.getHybridization()
+                                            .ordinal();
+            this.atomProperties[i][2] = atom.getImplicitHydrogenCount();
+            this.atomProperties[i][3] = atom.getValency();
+            this.atomProperties[i][4] = atom.getFormalCharge();
+            this.atomProperties[i][5] = atom.isInRing()
+                                        ? 1
+                                        : 0;
+            this.atomProperties[i][6] = atom.isAromatic()
+                                        ? 1
+                                        : 0;
         }
-        this.updateBondCount();
-    }
-
-    private void setAtomProperties(final int atomIndex, final String atomType, final Integer implicitHydrogenCount,
-                                   final Integer valency, final Integer formalCharge, final Boolean isInRing,
-                                   final Boolean isAromatic, final Hybridization hybridization) {
-        this.atomTypes[atomIndex] = atomType;
-        this.atomProperties[atomIndex] = new Integer[5];
-        this.atomProperties[atomIndex][0] = implicitHydrogenCount;
-        this.atomProperties[atomIndex][1] = valency;
-        this.atomProperties[atomIndex][2] = formalCharge;
-        this.atomProperties[atomIndex][3] = isInRing
-                                            ? 1
-                                            : 0;
-        this.atomProperties[atomIndex][4] = isAromatic
-                                            ? 1
-                                            : 0;
-        this.hybridizations[atomIndex] = hybridization;
-    }
-
-    private void updateBondCount() {
-        int bondCounter = 0;
-        for (int i = 0; i
-                < this.adjacencyList.length; i++) {
-            bondCounter += this.adjacencyList[i].length;
-        }
-        this.bondCount = bondCounter
-                / 2;
     }
 
     public int getAtomCount() {
-        return this.adjacencyList.length;
+        return this.bondProperties.length;
     }
 
+    public int getBondCount() {
+        int bondCounter = 0;
+        for (int i = 0; i
+                < this.bondProperties.length; i++) {
+            bondCounter += this.bondProperties[i].length;
+        }
+        return bondCounter
+                / 2;
+    }
 
     public IAtomContainer toAtomContainer() {
         final IAtomContainer ac = SilentChemObjectBuilder.getInstance()
                                                          .newAtomContainer();
         IAtom atom;
         for (int i = 0; i
-                < this.adjacencyList.length; i++) {
-            if (this.atomTypes[i].equals("R")) {
+                < this.bondProperties.length; i++) {
+            if (this.atomProperties[i][0]
+                    == -1) {
                 atom = new PseudoAtom("R");
             } else {
-                atom = new Atom(this.atomTypes[i]);
+                atom = new Atom(this.atomProperties[i][0]);
             }
-            atom.setImplicitHydrogenCount(this.atomProperties[i][0]);
-            atom.setValency(this.atomProperties[i][1]);
-            atom.setFormalCharge(this.atomProperties[i][2]);
-            atom.setIsInRing(this.atomProperties[i][3]
+            atom.setHybridization(IAtomType.Hybridization.values()[this.atomProperties[i][1]]);
+            atom.setImplicitHydrogenCount(this.atomProperties[i][2]);
+            atom.setValency(this.atomProperties[i][3]);
+            atom.setFormalCharge(this.atomProperties[i][4]);
+            atom.setIsInRing(this.atomProperties[i][5]
                                      == 1);
-            atom.setIsAromatic(this.atomProperties[i][4]
+            atom.setIsAromatic(this.atomProperties[i][6]
                                        == 1);
-            atom.setHybridization(this.hybridizations[i]);
 
             ac.addAtom(atom);
         }
         IBond bond;
         for (int i = 0; i
-                < this.adjacencyList.length; i++) {
+                < this.bondProperties.length; i++) {
             for (int k = 0; k
-                    < this.adjacencyList[i].length; k++) {
-                if (ac.getBond(ac.getAtom(i), ac.getAtom(this.adjacencyList[i][k][0]))
+                    < this.bondProperties[i].length; k++) {
+                if (ac.getBond(ac.getAtom(i), ac.getAtom(this.bondProperties[i][k][0]))
                         == null) {
-                    bond = new Bond(ac.getAtom(i), ac.getAtom(this.adjacencyList[i][k][0]),
-                                    Utils.getBondOrder(this.adjacencyList[i][k][1]));
-                    bond.setIsInRing(this.adjacencyList[i][k][2]
+                    bond = new Bond(ac.getAtom(i), ac.getAtom(this.bondProperties[i][k][0]),
+                                    Utils.getBondOrder(this.bondProperties[i][k][1]));
+                    bond.setIsInRing(this.bondProperties[i][k][2]
                                              == 1);
-                    bond.setIsAromatic(this.adjacencyList[i][k][3]
+                    bond.setIsAromatic(this.bondProperties[i][k][3]
                                                == 1);
                     ac.addBond(bond);
                 }
@@ -173,15 +163,11 @@ public class ExtendedAdjacencyList {
                 + "atomCount="
                 + this.getAtomCount()
                 + ", bondCount="
-                + this.bondCount
-                + ", adjacencyList="
-                + Arrays.deepToString(this.adjacencyList)
-                + ", atomTypes="
-                + Arrays.toString(this.atomTypes)
+                + this.getBondCount()
+                + ", bondProperties="
+                + Arrays.deepToString(this.bondProperties)
                 + ", atomProperties="
                 + Arrays.deepToString(this.atomProperties)
-                + ", hybridizations="
-                + Arrays.toString(this.hybridizations)
                 + '}';
     }
 }

145c1c700403140003442e671668f7586d167921
michaelwenk <michael.wenk@uni-jena.de>
2021-08-27 17:37:04 +0200

    chore: extracted buildConnectivityStatistics method

--- src/casekit/nmr/analysis/ConnectivityStatistics.java
+++ src/casekit/nmr/analysis/ConnectivityStatistics.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectivityStatistics {
 
@@ -21,92 +22,97 @@ public class ConnectivityStatistics {
      */
     public static void buildConnectivityStatistics(final List<DataSet> dataSetList, final String nucleus,
                                                    final Map<String, Map<String, Map<Integer, Map<String, Map<String, Map<Integer, Integer>>>>>> connectivityStatistics) {
-        IAtomContainer structure;
-        Spectrum spectrum;
-        IAtom atom;
         final String atomType = Utils.getAtomTypeFromNucleus(nucleus);
-        String multiplicity;
-        String hybridization;
-        String connectedAtomType;
-        String connectedAtomHybridization;
-        int shift, atomIndex;
         for (final DataSet dataSet : dataSetList) {
             if (!dataSet.getSpectrum()
                         .getNuclei()[0].equals(nucleus)) {
                 continue;
             }
-            structure = dataSet.getStructure()
-                               .toAtomContainer();
-            spectrum = dataSet.getSpectrum()
-                              .toSpectrum();
-            for (int signalIndex = 0; signalIndex
-                    < spectrum.getSignalCount(); signalIndex++) {
-                shift = spectrum.getShift(signalIndex, 0)
-                                .intValue();
-                for (int equivalenceIndex = 0; equivalenceIndex
-                        < dataSet.getAssignment()
-                                 .getAssignment(0, signalIndex).length; equivalenceIndex++) {
-                    atomIndex = dataSet.getAssignment()
-                                       .getAssignment(0, signalIndex, equivalenceIndex);
-                    atom = structure.getAtom(atomIndex);
-                    if (atom.getSymbol()
-                            .equals(atomType)) {
-                        multiplicity = Utils.getMultiplicityFromProtonsCount(atom.getImplicitHydrogenCount());
-                        if (multiplicity
+            buildConnectivityStatistics(dataSet, atomType, connectivityStatistics);
+        }
+    }
+
+    /**
+     * @param dataSet
+     * @param atomType
+     * @param connectivityStatistics multiplicity -> hybridization -> shift (int) -> connected atom symbol -> connected atom hybridization -> connected atom protons count -> occurrence
+     */
+    public static void buildConnectivityStatistics(final DataSet dataSet, final String atomType,
+                                                   final Map<String, Map<String, Map<Integer, Map<String, Map<String, Map<Integer, Integer>>>>>> connectivityStatistics) {
+        final IAtomContainer structure = dataSet.getStructure()
+                                                .toAtomContainer();
+        final Spectrum spectrum = dataSet.getSpectrum()
+                                         .toSpectrum();
+        int shift, atomIndex;
+        IAtom atom;
+        String multiplicity, hybridization, connectedAtomType, connectedAtomHybridization;
+        for (int signalIndex = 0; signalIndex
+                < spectrum.getSignalCount(); signalIndex++) {
+            shift = spectrum.getShift(signalIndex, 0)
+                            .intValue();
+            for (int equivalenceIndex = 0; equivalenceIndex
+                    < dataSet.getAssignment()
+                             .getAssignment(0, signalIndex).length; equivalenceIndex++) {
+                atomIndex = dataSet.getAssignment()
+                                   .getAssignment(0, signalIndex, equivalenceIndex);
+                atom = structure.getAtom(atomIndex);
+                if (atom.getSymbol()
+                        .equals(atomType)) {
+                    multiplicity = Utils.getMultiplicityFromProtonsCount(atom.getImplicitHydrogenCount());
+                    if (multiplicity
+                            == null) {
+                        continue;
+                    }
+                    multiplicity = multiplicity.toLowerCase();
+                    hybridization = atom.getHybridization()
+                                        .name();
+                    connectivityStatistics.putIfAbsent(multiplicity, new ConcurrentHashMap<>());
+                    connectivityStatistics.get(multiplicity)
+                                          .putIfAbsent(hybridization, new ConcurrentHashMap<>());
+                    // check for connected hetero atoms
+                    for (final IAtom connectedAtom : structure.getConnectedAtomsList(atom)) {
+                        if (connectedAtom.getSymbol()
+                                         .equals("H")) {
+                            continue;
+                        }
+                        connectedAtomType = connectedAtom.getSymbol();
+                        if (connectedAtom.getHybridization()
                                 == null) {
                             continue;
                         }
-                        multiplicity = multiplicity.toLowerCase();
-                        hybridization = atom.getHybridization()
-                                            .name();
-                        connectivityStatistics.putIfAbsent(multiplicity, new HashMap<>());
+                        connectedAtomHybridization = connectedAtom.getHybridization()
+                                                                  .name();
                         connectivityStatistics.get(multiplicity)
-                                              .putIfAbsent(hybridization, new HashMap<>());
-                        // check for connected hetero atoms
-                        for (final IAtom connectedAtom : structure.getConnectedAtomsList(atom)) {
-                            if (connectedAtom.getSymbol()
-                                             .equals("H")) {
-                                continue;
-                            }
-                            connectedAtomType = connectedAtom.getSymbol();
-                            if (connectedAtom.getHybridization()
-                                    == null) {
-                                continue;
-                            }
-                            connectedAtomHybridization = connectedAtom.getHybridization()
-                                                                      .name();
-                            connectivityStatistics.get(multiplicity)
-                                                  .get(hybridization)
-                                                  .putIfAbsent(shift, new HashMap<>());
-                            connectivityStatistics.get(multiplicity)
-                                                  .get(hybridization)
-                                                  .get(shift)
-                                                  .putIfAbsent(connectedAtomType, new HashMap<>());
-                            connectivityStatistics.get(multiplicity)
-                                                  .get(hybridization)
-                                                  .get(shift)
-                                                  .get(connectedAtomType)
-                                                  .putIfAbsent(connectedAtomHybridization, new HashMap<>());
-                            connectivityStatistics.get(multiplicity)
-                                                  .get(hybridization)
-                                                  .get(shift)
-                                                  .get(connectedAtomType)
-                                                  .get(connectedAtomHybridization)
-                                                  .putIfAbsent(connectedAtom.getImplicitHydrogenCount(), 0);
-                            connectivityStatistics.get(multiplicity)
-                                                  .get(hybridization)
-                                                  .get(shift)
-                                                  .get(connectedAtomType)
-                                                  .get(connectedAtomHybridization)
-                                                  .put(connectedAtom.getImplicitHydrogenCount(),
-                                                       connectivityStatistics.get(multiplicity)
-                                                                             .get(hybridization)
-                                                                             .get(shift)
-                                                                             .get(connectedAtomType)
-                                                                             .get(connectedAtomHybridization)
-                                                                             .get(connectedAtom.getImplicitHydrogenCount())
-                                                               + 1);
-                        }
+                                              .get(hybridization)
+                                              .putIfAbsent(shift, new ConcurrentHashMap<>());
+                        connectivityStatistics.get(multiplicity)
+                                              .get(hybridization)
+                                              .get(shift)
+                                              .putIfAbsent(connectedAtomType, new ConcurrentHashMap<>());
+                        connectivityStatistics.get(multiplicity)
+                                              .get(hybridization)
+                                              .get(shift)
+                                              .get(connectedAtomType)
+                                              .putIfAbsent(connectedAtomHybridization, new ConcurrentHashMap<>());
+                        connectivityStatistics.get(multiplicity)
+                                              .get(hybridization)
+                                              .get(shift)
+                                              .get(connectedAtomType)
+                                              .get(connectedAtomHybridization)
+                                              .putIfAbsent(connectedAtom.getImplicitHydrogenCount(), 0);
+                        connectivityStatistics.get(multiplicity)
+                                              .get(hybridization)
+                                              .get(shift)
+                                              .get(connectedAtomType)
+                                              .get(connectedAtomHybridization)
+                                              .put(connectedAtom.getImplicitHydrogenCount(), connectivityStatistics.get(
+                                                      multiplicity)
+                                                                                                                   .get(hybridization)
+                                                                                                                   .get(shift)
+                                                                                                                   .get(connectedAtomType)
+                                                                                                                   .get(connectedAtomHybridization)
+                                                                                                                   .get(connectedAtom.getImplicitHydrogenCount())
+                                                      + 1);
                     }
                 }
             }

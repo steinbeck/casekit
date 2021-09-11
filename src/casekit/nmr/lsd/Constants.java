168277e7a0f521761c5e510ff37da59a22779267
michaelwenk <michael.wenk@uni-jena.de>
2021-09-11 12:16:39 +0200

    chore: simplification in Constants

--- src/casekit/nmr/lsd/Constants.java
+++ src/casekit/nmr/lsd/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
     public static final Map<String, int[]> defaultHybridizationMap = createDefaultHybridizationMap();
     public static final Map<String, int[]> defaultProtonsCountPerValencyMap = createDefaultProtonsCountPerValencyMap();
     public static final Map<String, String> defaultAtomLabelMap = createDefaultAtomLabelMap();
-    public static final Map<String, Map<String, Integer>> hybridizationConversionMap = createHybridizationConversionMap();
+    public static final Map<String, Integer> hybridizationConversionMap = createHybridizationConversionMap();
 
     private static Map<String, String> createNucleiMap() {
         final Map<String, String> nuclei = new HashMap<>();
@@ -63,51 +63,15 @@ public class Constants {
         return Collections.unmodifiableMap(defaultAtomLabel);
     }
 
-    private static Map<String, Map<String, Integer>> createHybridizationConversionMap() {
+    private static Map<String, Integer> createHybridizationConversionMap() {
         // @TODO access this information from MongoDB and store it instead of hard coding it
         // possible command in MongoDB: db.hybridizations.aggregate([{$match: {nucleus: "15N"}}, {$group: {_id: null, set: {$addToSet: "$hybridization"}}}])
         // nucleus -> hybridization string -> number
-        final Map<String, Map<String, Integer>> hybridizationConversionMap = new HashMap<>();
-        hybridizationConversionMap.put("C", new HashMap<>());
-        hybridizationConversionMap.get("C")
-                                  .put("PLANAR3", 3);
-        hybridizationConversionMap.get("C")
-                                  .put("SP3", 3);
-        hybridizationConversionMap.get("C")
-                                  .put("SP2", 2);
-        hybridizationConversionMap.get("C")
-                                  .put("SP1", 1);
-        hybridizationConversionMap.put("N", new HashMap<>());
-        hybridizationConversionMap.get("N")
-                                  .put("PLANAR3", 3);
-        hybridizationConversionMap.get("N")
-                                  .put("SP3", 3);
-        hybridizationConversionMap.get("N")
-                                  .put("SP2", 2);
-        hybridizationConversionMap.get("N")
-                                  .put("SP1", 1);
-        hybridizationConversionMap.put("S", new HashMap<>());
-        hybridizationConversionMap.get("S")
-                                  .put("PLANAR3", 3);
-        hybridizationConversionMap.get("S")
-                                  .put("SP3", 3);
-        hybridizationConversionMap.get("S")
-                                  .put("SP2", 2);
-        hybridizationConversionMap.get("S")
-                                  .put("SP1", 1);
-        hybridizationConversionMap.put("O", new HashMap<>());
-        hybridizationConversionMap.get("O")
-                                  .put("PLANAR3", 3);
-        hybridizationConversionMap.get("O")
-                                  .put("SP3", 3);
-        hybridizationConversionMap.get("O")
-                                  .put("SP2", 2);
-        hybridizationConversionMap.put("I", new HashMap<>());
-        hybridizationConversionMap.get("I")
-                                  .put("SP3", 3);
-        hybridizationConversionMap.put("F", new HashMap<>());
-        hybridizationConversionMap.get("F")
-                                  .put("SP3", 3);
+        final Map<String, Integer> hybridizationConversionMap = new HashMap<>();
+        hybridizationConversionMap.put("PLANAR3", 3);
+        hybridizationConversionMap.put("SP3", 3);
+        hybridizationConversionMap.put("SP2", 2);
+        hybridizationConversionMap.put("SP1", 1);
 
         return Collections.unmodifiableMap(hybridizationConversionMap);
     }

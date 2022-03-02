e47c3f49d6cdf319ac035221aa5c7a58333195d8
michaelwenk <michael.wenk@uni-jena.de>
2022-03-02 18:28:19 +0100

    chore: do not store molfiles during filtering and ranking & store similarity values in DataSet attachment and not meta

--- src/casekit/nmr/filterandrank/FilterAndRank.java
+++ src/casekit/nmr/filterandrank/FilterAndRank.java
@@ -6,11 +6,8 @@ import casekit.nmr.model.DataSet;
 import casekit.nmr.model.Spectrum;
 import casekit.nmr.similarity.Similarity;
 import casekit.nmr.utils.Statistics;
-import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.fingerprint.BitSetFingerprint;
-import org.openscience.cdk.io.MDLV3000Writer;
 
-import java.io.ByteArrayOutputStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -27,7 +24,7 @@ public class FilterAndRank {
                            checkEquivalencesCount, multiplicitySectionsBuilder, allowIncompleteMatch));
     }
 
-    public static List<DataSet> filter(List<DataSet> dataSetList, final Spectrum querySpectrum,
+    public static List<DataSet> filter(final List<DataSet> dataSetList, final Spectrum querySpectrum,
                                        final double shiftTolerance, final double maxAverageDeviation,
                                        final boolean checkMultiplicity, final boolean checkEquivalencesCount,
                                        final MultiplicitySectionsBuilder multiplicitySectionsBuilder,
@@ -35,15 +32,13 @@ public class FilterAndRank {
         if (querySpectrum.getNDim()
                 == 1
                 && querySpectrum.getNuclei()[0].equals("13C")) {
-            // @TODO get shift tolerance as arguments
-
-            dataSetList = dataSetList.stream()
-                                     .filter(dataSet -> checkDataSet(dataSet, querySpectrum, shiftTolerance,
-                                                                     maxAverageDeviation, checkMultiplicity,
-                                                                     checkEquivalencesCount,
-                                                                     multiplicitySectionsBuilder, allowIncompleteMatch)
-                                             != null)
-                                     .collect(Collectors.toList());
+            return dataSetList.stream()
+                              .filter(dataSet -> checkDataSet(dataSet, querySpectrum, shiftTolerance,
+                                                              maxAverageDeviation, checkMultiplicity,
+                                                              checkEquivalencesCount, multiplicitySectionsBuilder,
+                                                              allowIncompleteMatch)
+                                      != null)
+                              .collect(Collectors.toList());
         }
 
         return dataSetList;
@@ -54,67 +49,52 @@ public class FilterAndRank {
                                        final boolean checkEquivalencesCount,
                                        final MultiplicitySectionsBuilder multiplicitySectionsBuilder,
                                        final boolean allowIncompleteMatch) {
-        try {
-            final BitSetFingerprint bitSetFingerprintQuerySpectrum = Similarity.getBitSetFingerprint(querySpectrum, 0,
-                                                                                                     multiplicitySectionsBuilder);
-            final Spectrum spectrum = dataSet.getSpectrum()
-                                             .toSpectrum();
-            final Assignment spectralMatchAssignment = Similarity.matchSpectra(spectrum, querySpectrum, 0, 0,
-                                                                               shiftTolerance, checkMultiplicity,
-                                                                               checkEquivalencesCount, false);
+        final BitSetFingerprint bitSetFingerprintQuerySpectrum = Similarity.getBitSetFingerprint(querySpectrum, 0,
+                                                                                                 multiplicitySectionsBuilder);
+        final Spectrum spectrum = dataSet.getSpectrum()
+                                         .toSpectrum();
+        final Assignment spectralMatchAssignment = Similarity.matchSpectra(spectrum, querySpectrum, 0, 0,
+                                                                           shiftTolerance, checkMultiplicity,
+                                                                           checkEquivalencesCount, false);
 
-            dataSet.addMetaInfo("querySpectrumSignalCount", String.valueOf(querySpectrum.getSignalCount()));
-            dataSet.addMetaInfo("querySpectrumSignalCountWithEquivalences",
-                                String.valueOf(querySpectrum.getSignalCountWithEquivalences()));
-            dataSet.addMetaInfo("setAssignmentsCountWithEquivalences",
-                                String.valueOf(spectralMatchAssignment.getSetAssignmentsCountWithEquivalences(0)));
-            final boolean isCompleteSpectralMatch = querySpectrum.getSignalCount()
-                    == spectralMatchAssignment.getSetAssignmentsCount(0);
-            final boolean isCompleteSpectralMatchWithEquivalences = querySpectrum.getSignalCountWithEquivalences()
-                    == spectralMatchAssignment.getSetAssignmentsCountWithEquivalences(0);
-            dataSet.addMetaInfo("setAssignmentsCount",
-                                String.valueOf(spectralMatchAssignment.getSetAssignmentsCount(0)));
-            dataSet.addMetaInfo("setAssignmentsCountWithEquivalences",
-                                String.valueOf(spectralMatchAssignment.getSetAssignmentsCountWithEquivalences(0)));
-            dataSet.addMetaInfo("isCompleteSpectralMatch", String.valueOf(isCompleteSpectralMatch));
-            dataSet.addMetaInfo("isCompleteSpectralMatchWithEquivalences",
-                                String.valueOf(isCompleteSpectralMatchWithEquivalences));
-            dataSet.addAttachment("spectralMatchAssignment", spectralMatchAssignment);
+        dataSet.addAttachment("querySpectrumSignalCount", querySpectrum.getSignalCount());
+        dataSet.addAttachment("querySpectrumSignalCountWithEquivalences",
+                              querySpectrum.getSignalCountWithEquivalences());
+        dataSet.addAttachment("setAssignmentsCountWithEquivalences",
+                              spectralMatchAssignment.getSetAssignmentsCountWithEquivalences(0));
+        final boolean isCompleteSpectralMatch = querySpectrum.getSignalCount()
+                == spectralMatchAssignment.getSetAssignmentsCount(0);
+        final boolean isCompleteSpectralMatchWithEquivalences = querySpectrum.getSignalCountWithEquivalences()
+                == spectralMatchAssignment.getSetAssignmentsCountWithEquivalences(0);
+        dataSet.addAttachment("setAssignmentsCount", spectralMatchAssignment.getSetAssignmentsCount(0));
+        dataSet.addAttachment("setAssignmentsCountWithEquivalences",
+                              spectralMatchAssignment.getSetAssignmentsCountWithEquivalences(0));
+        dataSet.addAttachment("isCompleteSpectralMatch", isCompleteSpectralMatch);
+        dataSet.addAttachment("isCompleteSpectralMatchWithEquivalences", isCompleteSpectralMatchWithEquivalences);
+        dataSet.addAttachment("spectralMatchAssignment", spectralMatchAssignment);
 
+        Double[] deviations = Similarity.getDeviations(spectrum, querySpectrum, 0, 0, spectralMatchAssignment);
+        if (allowIncompleteMatch) {
+            deviations = Arrays.stream(deviations)
+                               .filter(Objects::nonNull)
+                               .toArray(Double[]::new);
+        }
+        final Double averageDeviation = Statistics.calculateAverageDeviation(deviations);
+        if (averageDeviation
+                != null
+                && averageDeviation
+                <= maxAverageDeviation) {
+            dataSet.addAttachment("averageDeviation", averageDeviation);
+            final Double rmsd = Statistics.calculateRMSD(deviations);
+            dataSet.addAttachment("rmsd", rmsd);
 
-            // store as MOL file
-            final MDLV3000Writer mdlv3000Writer = new MDLV3000Writer();
-            final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            mdlv3000Writer.setWriter(byteArrayOutputStream);
-            mdlv3000Writer.write(dataSet.getStructure()
-                                        .toAtomContainer());
-            dataSet.addMetaInfo("molfile", byteArrayOutputStream.toString());
+            final BitSetFingerprint bitSetFingerprintDataSet = Similarity.getBitSetFingerprint(spectrum, 0,
+                                                                                               multiplicitySectionsBuilder);
+            final Double tanimotoCoefficient = Similarity.calculateTanimotoCoefficient(bitSetFingerprintQuerySpectrum,
+                                                                                       bitSetFingerprintDataSet);
+            dataSet.addAttachment("tanimoto", tanimotoCoefficient);
 
-            Double[] deviations = Similarity.getDeviations(spectrum, querySpectrum, 0, 0, spectralMatchAssignment);
-            if (allowIncompleteMatch) {
-                deviations = Arrays.stream(deviations)
-                                   .filter(Objects::nonNull)
-                                   .toArray(Double[]::new);
-            }
-            final Double averageDeviation = Statistics.calculateAverageDeviation(deviations);
-            if (averageDeviation
-                    != null
-                    && averageDeviation
-                    <= maxAverageDeviation) {
-                dataSet.addMetaInfo("averageDeviation", String.valueOf(averageDeviation));
-                final Double rmsd = Statistics.calculateRMSD(deviations);
-                dataSet.addMetaInfo("rmsd", String.valueOf(rmsd));
-
-                final BitSetFingerprint bitSetFingerprintDataSet = Similarity.getBitSetFingerprint(spectrum, 0,
-                                                                                                   multiplicitySectionsBuilder);
-                final Double tanimotoCoefficient = Similarity.calculateTanimotoCoefficient(
-                        bitSetFingerprintQuerySpectrum, bitSetFingerprintDataSet);
-                dataSet.addMetaInfo("tanimoto", String.valueOf(tanimotoCoefficient));
-
-                return dataSet;
-            }
-        } catch (final CDKException e) {
-            e.printStackTrace();
+            return dataSet;
         }
 
         return null;
@@ -122,7 +102,7 @@ public class FilterAndRank {
 
     public static List<DataSet> rank(final List<DataSet> dataSetList) {
         dataSetList.sort((dataSet1, dataSet2) -> {
-            final int avgDevComparison = compareNumericDataSetMetaKey(dataSet1, dataSet2, "averageDeviation");
+            final int avgDevComparison = compareNumericDataSetAttachmentKey(dataSet1, dataSet2, "averageDeviation");
             if (avgDevComparison
                     != 0) {
                 return avgDevComparison;
@@ -139,8 +119,8 @@ public class FilterAndRank {
                     && isCompleteSpectralMatchDataSet2) {
                 return 1;
             }
-            final int setAssignmentsCountComparison = compareNumericDataSetMetaKey(dataSet1, dataSet2,
-                                                                                   "setAssignmentsCount");
+            final int setAssignmentsCountComparison = compareNumericDataSetAttachmentKey(dataSet1, dataSet2,
+                                                                                         "setAssignmentsCount");
             if (setAssignmentsCountComparison
                     != 0) {
                 return -1
@@ -153,19 +133,19 @@ public class FilterAndRank {
         return dataSetList;
     }
 
-    private static int compareNumericDataSetMetaKey(final DataSet dataSet1, final DataSet dataSet2,
-                                                    final String metaKey) {
+    private static int compareNumericDataSetAttachmentKey(final DataSet dataSet1, final DataSet dataSet2,
+                                                          final String attachmentKey) {
         Double valueDataSet1 = null;
         Double valueDataSet2 = null;
         try {
-            valueDataSet1 = Double.parseDouble(dataSet1.getMeta()
-                                                       .get(metaKey));
+            valueDataSet1 = (Double) dataSet1.getAttachment()
+                                             .get(attachmentKey);
         } catch (final NullPointerException | NumberFormatException e) {
             //                e.printStackTrace();
         }
         try {
-            valueDataSet2 = Double.parseDouble(dataSet2.getMeta()
-                                                       .get(metaKey));
+            valueDataSet2 = (Double) dataSet2.getAttachment()
+                                             .get(attachmentKey);
         } catch (final NullPointerException | NumberFormatException e) {
             //                e.printStackTrace();
         }
